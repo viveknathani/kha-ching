735d48c0c28691baf2caa562bb0765cb06d6b5d1
Aakash Goel <aakash.lpin@gmail.com>
2024-04-01 14:58:27 +0530

    Update order_history.js

--- pages/api/order_history.js
+++ pages/api/order_history.js
@@ -1,3 +1,5 @@
+import { BrokerName } from 'inves-broker'
+import getInvesBrokerInstance from '../../lib/invesBroker'
 import withSession from '../../lib/session'
 import { syncGetKiteInstance } from '../../lib/utils'
 
