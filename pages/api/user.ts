369694ccd32c6edb3a5780c8463499b47c729edd
aakashlpin <aakash.lpin@gmail.com>
2024-04-05 10:56:53 +0530

    log

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -8,16 +8,22 @@ const apiKey = process.env.KITE_API_KEY
 export default withSession(async (req, res) => {
   const user: SignalXUser = req.session.get('user')
 
+  console.log('user', user)
+
   if (user) {
     const kc = new KiteConnect({
       api_key: apiKey,
       access_token: user?.session?.access_token
     })
 
+    console.log("kc done");
+
     try {
       // see if we're able to fetch profile with the access token
       // in case access token is expired, then log out the user
       await kc.getProfile()
+
+      console.log("profile fetch done");
 
       res.json({
         ...user,
@@ -30,6 +36,7 @@ export default withSession(async (req, res) => {
       })
     }
   } else {
+    console.log('else condn')
     res.json({
       isLoggedIn: false
     })
